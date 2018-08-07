fe5be697d272dfc75efdfb4f838822db8bf3c331
Andrew Regier <aregier@regiernet.com>
2018-08-07 09:47:00 -0500

    add missing export; explicitly note the index file under dist

--- source/index.ts
+++ source/index.ts
@@ -1,5 +1,6 @@
 export * from './ApiCredentials';
 export * from './BearerTokenCredentials';
+export * from './DiscoverySdk';
 export * from './DiscoveryServiceApi';
 export * from './IAMCredentials';
 export * from './ServiceApiModel';
