312496a73eb7fd267210e184e6b90fa1eb9f3061
Andrew Regier <aregier@regiernet.com>
2018-08-15 09:55:37 -0500

    Fix arguments for sed

--- pipeline/run-system-tests.sh
+++ pipeline/run-system-tests.sh
@@ -1,3 +1,3 @@
 sed -e "s|AWS_REGION|$AWS_REGION|" test/system/config.ts.sample > test/system/config.ts
-sed -e "s|DISCOVERY_SERVICE_ENDPOINT|$DISCOVERY_SERVICE_ENDPOINT|" test/system/config.ts.sample > test/system/config.ts
+sed -i -e "s|DISCOVERY_SERVICE_ENDPOINT|$DISCOVERY_SERVICE_ENDPOINT|" test/system/config.ts.sample > test/system/config.ts
 npm run-script system-test
