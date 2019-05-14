8a655adfdac70f78c712837762423600158aece1
Tim Rupe <trupe@aais.com>
2019-05-14 13:50:53 -0500

    Starting cloud dependency config

--- source/DiscoverySdk.ts
+++ source/DiscoverySdk.ts
@@ -1,19 +1,66 @@
 import { ServiceApiModel } from './ServiceApiModel';
 import { DiscoveryServiceApi } from './DiscoveryServiceApi';
+import * as finder from 'find-package-json';
+import * as fs from 'fs';
+
+export interface ICloudDependency {
+    name: string;
+    version: string;
+}
 
 export class DiscoverySdk {
     private api: DiscoveryServiceApi;
     private defaultStageName: string;
+    private cloudDependencies = new Map<string, ICloudDependency>();
 
-    constructor(serviceEndpointUri: string, region: string = 'us-east-1', defaultStageName: string = '') {
+    constructor(serviceEndpointUri: string,
+                region: string = 'us-east-1',
+                defaultStageName: string = '',
+                configPath: string = '') {
         this.api = new DiscoveryServiceApi(serviceEndpointUri, region, { type: 'None' });
         // TODO: attempt to look up stage name from an environment variable
         this.defaultStageName = defaultStageName;
+        this.populateDependencies(configPath);
     }
 
-    public async lookupService(serviceName: string, stageName: string = this.defaultStageName, version: string = '', externalID: string = '') {
+    get cloudDependencyNames() {
+        return Array.from(this.cloudDependencies.keys());
+    }
+
+    public getDependency(name: string) {
+        return this.cloudDependencies.get(name);
+    }
+
+    public async lookupService(serviceName: string,
+                               stageName: string = this.defaultStageName,
+                               version: string = '',
+                               externalID: string = '') {
         const result = await this.api.lookupService(serviceName, stageName, version, externalID);
-        const matches = result.data;
         return result.data.map((item: ServiceApiModel) => item.ServiceURL);
+    }
+
+    private populateDependencies(configPath: string) {
+        if (configPath.length > 0) {
+            this.readConfig(configPath);
+        } else {
+            // Look for the package.json file in the root project folder
+            const basePackagePath = finder(module).next().filename;
+            if (basePackagePath) {
+                this.readConfig(basePackagePath);
+            }
+        }
+    }
+
+    private readConfig(configPath: string) {
+        const rawdata = fs.readFileSync(configPath);
+        const config = JSON.parse(rawdata.toString('utf8'));
+
+        if (config.hasOwnProperty('cloudDependencies')) {
+            for (const key in config.cloudDependencies) {
+                if (config.cloudDependencies.hasOwnProperty(key)) {
+                    this.cloudDependencies.set(key, {name: key, version: config.cloudDependencies.key});
+                }
+            }
+        }
     }
 }
